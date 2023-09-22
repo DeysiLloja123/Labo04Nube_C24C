c7d9a5375dc6f4b14e7ae496156d0ef2389715bd
DeysiLloja123 <llojadeysirubi@>
2023-09-21 13:31:00 -1200

    Primer commit

---
+++ Lab04_DeysiLloja/index.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const app = express();
+//indicando que se cargen archivos static
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => { 
+    res.sendFile('./static/inicio.html',{
+        root:__dirname
+    })
+})
+app.get('/clientes', (req, res) => { 
+    res.sendFile('./static/clientes.html',{
+        root:__dirname
+    })
+})
+app.get('/productos', (req, res) => { 
+    res.sendFile('./static/productos.html',{
+        root:__dirname
+    })
+})
+
+app.listen(5000)
+console.log(`server on port ${5000}`)
+
